92ce81eb5ec1a7f075496505b63334961a886e27
mmonk3 <lawluker@gmail.com>
2022-09-02 14:30:45 +0500

    Hardcodes remooved

--- Test.py
+++ Test.py
@@ -7,10 +7,8 @@ import Autorization
 import Qase
 
 
-#code = os.environ['CODE']
-#plan_id = os.environ['PLAN_ID']
-code = 'W370'
-plan_id = '2'
+code = os.environ['CODE']
+plan_id = os.environ['PLAN_ID']
 
 
 @pytest.mark.parametrize('udid', Autorization.udid.values(), ids=Autorization.udid)
